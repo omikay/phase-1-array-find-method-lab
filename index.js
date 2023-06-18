7cca496c74e6a3b91a2a06f0b619673a7dd218d4
Omid Kayhani <o.kayhani@gmail.com>
2023-06-18 13:36:36 +0000

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(array) {
+    const winInstance = array.find(element => element.result === "W");
+    if (winInstance) {
+        return winInstance.year;
+    } else {
+        return undefined;
+    }
+}
